e898b9c701a631142fae07f16a9fb8d58482eebc
andrew-raphael-lukasik <andrew.r.lukasik@gmail.com>
2025-10-01 17:59:36 +0200

    more functions

--- Assets/-Code/ServerAndClient/GameGrid.cs
+++ Assets/-Code/ServerAndClient/GameGrid.cs
@@ -11,6 +11,32 @@ namespace ServerAndClient
 {
     public static class GameGrid
     {
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int ToIndex (uint2 coord, uint2 mapSize)
+        {
+            coord = math.min(coord, mapSize-1);// clamp to map size
+            return (int)(coord.y * mapSize.x + coord.x);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int ToIndex (int x, int y, uint2 mapSize) => ToIndex(new uint2((uint)x, (uint)y), mapSize);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int ToIndex (uint x, uint y, uint2 mapSize) => ToIndex(new uint2(x, y), mapSize);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static uint2 ToCoord (float3 point, float3 mapOrigin, uint2 mapSize)
+        {
+            float3 localPoint = point - mapOrigin;
+            uint2 coord = (uint2)(new float2(localPoint.x, localPoint.z) / new float2(MapSettingsSingleton.CellSize, MapSettingsSingleton.CellSize));
+            coord = math.min(coord, mapSize-1);// clamp to map size
+            return coord;
+        }
+
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool Raycast (Ray ray, float3 mapOrigin, uint2 mapSize, out uint2 coord)
         {
@@ -18,10 +44,7 @@ namespace ServerAndClient
             if (plane.Raycast(ray, out float dist))
             {
                 float3 hit = ray.origin + ray.direction * dist;
-                float3 localPos = hit - mapOrigin;
-
-                coord = (uint2)(new float2(localPos.x, localPos.z) / new float2(MapSettingsSingleton.CellSize, MapSettingsSingleton.CellSize));
-                coord = math.min(coord, mapSize-1);// clamp to map size
+                coord = ToCoord(point: hit, mapOrigin: mapOrigin, mapSize: mapSize);
                 return true;
             }
             else
@@ -68,6 +91,7 @@ namespace ServerAndClient
             );
         }
 
+
         [Unity.Burst.BurstCompile]
         public struct RaycastJob : IJob
         {
@@ -91,5 +115,7 @@ namespace ServerAndClient
                 }
             }
         }
+
+
     }
 }
