71982e51ddef7fa4f0be59240dc3a28be8eed651
andrew-raphael-lukasik <andrew.r.lukasik@gmail.com>
2025-10-03 12:37:26 +0200

    adds: `EditStateMapChangeSystem`

---
+++ Assets/-Code/Server/Gameplay/EditStateMapChangeSystem.cs
@@ -0,0 +1,117 @@
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Collections;
+using Unity.Jobs;
+
+using ServerAndClient;
+using ServerAndClient.Gameplay;
+using ServerAndClient.Input;
+using Server.Simulation;
+
+using Assert = UnityEngine.Assertions.Assert;
+
+namespace Server.Gameplay
+{
+    [WorldSystemFilter(WorldSystemFilterFlags.Default | WorldSystemFilterFlags.ServerSimulation)]
+    [UpdateInGroup(typeof(GameInitializationSystemGroup), OrderFirst = true)]
+    [RequireMatchingQueriesForUpdate]
+    [Unity.Burst.BurstCompile]
+    public partial struct EditStateMapChangeSystem : ISystem
+    {
+        public static FixedString64Bytes DebugName {get;} = nameof(EditStateMapChangeSystem);
+
+        [Unity.Burst.BurstCompile]
+        void ISystem.OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<PlayerInputSingleton>();
+            state.RequireForUpdate<MapSettingsSingleton>();
+            state.RequireForUpdate<GeneratedMapData>();
+            state.RequireForUpdate<GameState>();
+        }
+
+        [Unity.Burst.BurstCompile]
+        void ISystem.OnUpdate(ref SystemState state)
+        {
+            var gameState = SystemAPI.GetSingleton<GameState>();
+            if (gameState.State!=EGameState.EDIT) return;// EDIT state only
+
+            var playerInput = SystemAPI.GetSingleton<PlayerInputSingleton>();
+            if (playerInput.ExecuteStart==1)
+            {
+                var mapSettings = SystemAPI.GetSingleton<MapSettingsSingleton>();
+                if (GameGrid.Raycast(ray: playerInput.PointerRay, mapOrigin: mapSettings.Origin, mapSize: mapSettings.Size, out uint2 dstCoord))
+                {
+                    var em = state.EntityManager;
+                    var mapDataRef = SystemAPI.GetSingletonRW<GeneratedMapData>();
+                    var floorsRef = SystemAPI.GetSingletonRW<FloorsSingleton>();
+                    var unitsRef = SystemAPI.GetSingletonRW<UnitsSingleton>();
+                    Entity srcFloor = SystemAPI.GetSingleton<SelectedFloorSingleton>().Selected;
+                    Entity srcUnit = SystemAPI.GetSingleton<SelectedUnitSingleton>().Selected;
+
+                    JobHandle.CompleteAll(
+                        ref mapDataRef.ValueRW.Dependency,
+                        ref floorsRef.ValueRW.Dependency,
+                        ref unitsRef.ValueRW.Dependency
+                    );
+
+                    var floors = floorsRef.ValueRO.Lookup;
+                    var units = unitsRef.ValueRO.Lookup;
+
+                    #if UNITY_EDITOR || DEBUG
+                    Assert.IsTrue(em.Exists(srcFloor), $"{DebugName} can't execute swap action without a src floor entity - it is a source of srcCoord");
+                    Assert.IsTrue(floors.ContainsKey(dstCoord), $"{DebugName} can't execute swap action without a floors lookup having an entity at {dstCoord}");
+                    #endif
+
+                    uint2 srcCoord = em.GetComponentData<FloorCoord>(srcFloor);
+                    if (math.all(srcCoord==dstCoord)) return;// ignore, src and dst are the same
+                    Entity dstFloor = floors[dstCoord];
+
+                    #if UNITY_EDITOR || DEBUG
+                    if (em.Exists(srcFloor)) Assert.IsTrue(em.HasComponent<FloorCoord>(srcFloor), $"Floor {srcFloor} has no {FloorCoord.DebugName}");
+                    if (em.Exists(srcUnit)) Assert.IsTrue(em.HasComponent<UnitCoord>(srcUnit), $"Unit {srcUnit} has no {UnitCoord.DebugName}");
+                    #endif
+
+                    LocalToWorld srcLtw = em.GetComponentData<LocalToWorld>(srcFloor);
+                    LocalToWorld dstLtw = em.GetComponentData<LocalToWorld>(dstFloor);
+
+                    // swap floors:
+                    {
+                        em.SetComponentData(srcFloor, dstLtw);
+                        em.SetComponentData(dstFloor, srcLtw);
+                        {
+                            int srcIndex = GameGrid.ToIndex(srcCoord, mapSettings.Size);
+                            int dstIndex = GameGrid.ToIndex(dstCoord, mapSettings.Size);
+                            var floorTypesRW = mapDataRef.ValueRW.FloorArray;
+                            EFloorType srcType = floorTypesRW[srcIndex];
+                            floorTypesRW[srcIndex] = floorTypesRW[dstIndex];;
+                            floorTypesRW[dstIndex] = srcType;
+                        }
+
+                        SystemAPI.SetSingleton(new SelectedFloorSingleton{
+                            Selected = Entity.Null
+                        });
+                    }
+
+                    // swap units:
+                    {
+                        bool srcEntityExists = em.Exists(srcUnit);
+                        bool dstEntityExists = units.TryGetValue(dstCoord, out Entity dstEntity);
+
+                        if (srcEntityExists || dstEntityExists)
+                        {
+                            if (srcEntityExists) em.SetComponentData(srcUnit, dstLtw);
+                            if (dstEntityExists) em.SetComponentData(dstEntity, srcLtw);
+                        }
+
+                        SystemAPI.SetSingleton(new SelectedUnitSingleton{
+                            Selected = Entity.Null
+                        });
+                    }
+                }
+            }
+        }
+
+    }
+}
