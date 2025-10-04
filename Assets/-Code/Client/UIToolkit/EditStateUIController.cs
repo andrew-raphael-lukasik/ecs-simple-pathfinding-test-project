846dc383e20a82e5ebeac560bc8eb1539341da48
andrew-raphael-lukasik <andrew.r.lukasik@gmail.com>
2025-10-04 12:37:30 +0200

    rebuild on every parameter change

--- Assets/-Code/Client/UIToolkit/EditStateUIController.cs
+++ Assets/-Code/Client/UIToolkit/EditStateUIController.cs
@@ -26,24 +26,15 @@ namespace Client.UIToolkit
             var world = World.DefaultGameObjectInjectionWorld;
             if (world!=null && world.IsCreated)
             {
-                var entityManager = world.EntityManager;
-                _queryMapSettings = entityManager.CreateEntityQuery(ComponentType.ReadWrite<MapSettingsSingleton>());
+                var em = world.EntityManager;
+                _queryMapSettings = em.CreateEntityQuery(ComponentType.ReadWrite<MapSettingsSingleton>());
                 _queryMapSettings.TryGetSingleton(out _mapSettings);
             }
 
             root.For<Button>("enter-play-mode-button", (button) => {
                 button.clicked += () => {
                     Debug.Log("Button clicked -> requesting switch to play");
-                    var world = World.DefaultGameObjectInjectionWorld;
-                    var entityManager = world.EntityManager;
-                    
-                    _mapSettingsEntity = _queryMapSettings.GetSingletonEntity();
-                    entityManager.SetComponentData(_mapSettingsEntity, _mapSettings);
-                    entityManager.AddComponent<GenerateMapEntitiesRequest>(_mapSettingsEntity);
-                    
-                    entityManager.CreateSingleton(new GameState.ChangeRequest{
-                        State = EGameState.PLAY
-                    });
+                    StartGame();
                 };
             });
             root.For<IntegerField>("settings-seed", (field) => {
@@ -52,6 +43,9 @@ namespace Client.UIToolkit
                     uint newValueSafe = (uint) Mathf.Max(e.newValue, 1);
                     _mapSettings.Seed = newValueSafe;
                     field.SetValueWithoutNotify((int) newValueSafe);
+
+                    Debug.Log("Seed value changed -> regenerating the map");
+                    GenerateMapAnew();
                 });
             });
             root.For<Vector2IntField>("settings-map-size", (field) => {
@@ -60,6 +54,9 @@ namespace Client.UIToolkit
                     uint2 newValueSafe = math.clamp(new uint2((uint) e.newValue.x, (uint) e.newValue.y), new uint2(1, 1), new uint2(MapSettingsSingleton.Size_MAX, MapSettingsSingleton.Size_MAX));
                     _mapSettings.Size = new uint2(newValueSafe.x, newValueSafe.y);
                     field.SetValueWithoutNotify(new Vector2Int((int) newValueSafe.x, (int) newValueSafe.y));
+
+                    Debug.Log("Size value changed -> regenerating the map");
+                    GenerateMapAnew();
                 });
             });
             root.For<IntegerField>("settings-num-player-units", (field) => {
@@ -68,6 +65,9 @@ namespace Client.UIToolkit
                     ushort newValueSafe = (ushort) Mathf.Clamp(e.newValue, 1, MapSettingsSingleton.NumPlayerUnits_MAX);
                     _mapSettings.NumPlayerUnits = newValueSafe;
                     field.SetValueWithoutNotify(newValueSafe);
+
+                    Debug.Log("num player units value changed -> regenerating the map");
+                    GenerateMapAnew();
                 });
             });
             root.For<IntegerField>("settings-num-enemy-units", (field) => {
@@ -76,8 +76,28 @@ namespace Client.UIToolkit
                     ushort newValueSafe = (ushort) Mathf.Clamp(e.newValue, 1, MapSettingsSingleton.NumEnemyUnits_MAX);
                     _mapSettings.NumEnemyUnits = newValueSafe;
                     field.SetValueWithoutNotify(newValueSafe);
+
+                    Debug.Log("num enemy units value changed -> regenerating the map");
+                    GenerateMapAnew();
                 });
             });
         }
+
+        void GenerateMapAnew ()
+        {
+            var em = World.DefaultGameObjectInjectionWorld.EntityManager;
+            _mapSettingsEntity = _queryMapSettings.GetSingletonEntity();
+            em.SetComponentData(_mapSettingsEntity, _mapSettings);
+            em.AddComponent<GenerateMapEntitiesRequest>(_mapSettingsEntity);
+        }
+
+        void StartGame ()
+        {
+            var em = World.DefaultGameObjectInjectionWorld.EntityManager;
+            em.CreateSingleton(new GameState.ChangeRequest{
+                State = EGameState.PLAY
+            });
+        }
+
     }
 }
