ee3e8f5c378c86e498aaba41cf1bbb8bc8737781
andrew-raphael-lukasik <andrew.r.lukasik@gmail.com>
2025-10-10 14:05:53 +0200

    rename

--- Assets/-Code/ServerAndClient/MonoBehaviours/StateStartedGameObjectSystems.cs
+++ Assets/-Code/ServerAndClient/MonoBehaviours/StateStartedGameObjectSystems.cs
@@ -10,14 +10,14 @@ namespace ServerAndClient.MonoBehaviours
     [UpdateInGroup(typeof(InitializationSystemGroup), OrderFirst = true)]
     [RequireMatchingQueriesForUpdate]
     [Unity.Burst.BurstCompile]
-    partial struct SingletateStartedGameObjectInitializationSystem : ISystem
+    partial struct SingleStateStartedGameObjectInitializationSystem : ISystem
     {
         [Unity.Burst.BurstCompile]
         void ISystem.OnCreate(ref SystemState state)
         {
             var query = new EntityQueryBuilder(Allocator.Temp)
                 .WithAny<IsEditStateOnlyGameObject, IsPlayStateOnlyGameObject>()
-                .WithAbsent<IsInitialized>()
+                .WithAbsent<IsGameObjectEntityInitialized>()
                 .Build(ref state);
             state.RequireForUpdate(query);
         }
@@ -36,19 +36,19 @@ namespace ServerAndClient.MonoBehaviours
             {
                 GameObject go = entityManager.GetComponentObject<GameObject>(entity);
                 go.SetActive(gameState==EGameState.EDIT);
-                ecb.AddComponent<IsInitialized>(entity);
+                ecb.AddComponent<IsGameObjectEntityInitialized>(entity);
                 Debug.Log($"{go.name} -> SetActive({go.activeSelf}), gameState: {gameState}");
             }
             foreach (var (_, entity) in SystemAPI.Query<IsPlayStateOnlyGameObject>().WithEntityAccess())
             {
                 GameObject go = entityManager.GetComponentObject<GameObject>(entity);
                 go.SetActive(gameState==EGameState.PLAY);
-                ecb.AddComponent<IsInitialized>(entity);
+                ecb.AddComponent<IsGameObjectEntityInitialized>(entity);
                 Debug.Log($"{go.name} -> SetActive({go.activeSelf}), gameState: {gameState}");
             }
         }
 
-        struct IsInitialized : IComponentData {}
+        struct IsGameObjectEntityInitialized : IComponentData {}
     }
 
     [WorldSystemFilter(WorldSystemFilterFlags.ServerSimulation| WorldSystemFilterFlags.LocalSimulation | WorldSystemFilterFlags.ClientSimulation | WorldSystemFilterFlags.ThinClientSimulation | WorldSystemFilterFlags.Presentation)]
