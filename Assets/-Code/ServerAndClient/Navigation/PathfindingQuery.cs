5c2077524d2c50ce31508597070c8333884a6b43
andrew-raphael-lukasik <andrew.r.lukasik@gmail.com>
2025-10-14 17:16:08 +0200

    nothing

--- Assets/-Code/ServerAndClient/Navigation/PathfindingQuery.cs
+++ Assets/-Code/ServerAndClient/Navigation/PathfindingQuery.cs
@@ -1,10 +1,17 @@
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Collections;
 
 namespace ServerAndClient.Navigation
 {
     public struct PathfindingQuery : IComponentData
     {
         public uint2 Src, Dst;
+    }
+
+    public struct PathfindingQueryResult : IComponentData
+    {
+        public byte Success;
+        public NativeArray<uint2> Path;
     }
 }
